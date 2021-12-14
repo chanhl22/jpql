70ef65a7e91f7dcbab3c2451e30624bc53924915
chanhl22 <chanhlee@student.42seoul.kr>
2021-12-14 22:40:47 +0900

    서브 쿼리

---
+++ src/main/java/jpql/JpaMain_5.java
@@ -0,0 +1,47 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain_5 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory enf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = enf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setAge(10);
+            member.changeTeam(team);
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            String query = "select m from Member m where exists (select t from m.team t where t.name = '팀A')";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        enf.close();
+
+    }
+}
