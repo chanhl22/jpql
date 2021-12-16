9fe5c29964103d2361b2765828b72f94fa7412d7
chanhl22 <chanhlee@student.42seoul.kr>
2021-12-16 14:18:37 +0900

    경로표현식

---
+++ src/main/java/jpql/JpaMain_9.java
@@ -0,0 +1,49 @@
+package jpql;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain_9 {
+
+    public static void main(String[] args) {
+        EntityManagerFactory enf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = enf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+            Member member1 = new Member();
+            member1.setUsername("관리자1");
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("관리자2");
+            em.persist(member2);
+
+            em.flush();
+            em.clear();
+
+            String query = "select m.username From Team t join t.members m";
+            List<Team> result = em.createQuery(query, Team.class)
+                    .getResultList();
+
+            for (Team s : result) {
+                System.out.println("s = " + s);
+            }
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        enf.close();
+
+    }
+}
